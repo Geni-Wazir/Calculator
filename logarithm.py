f2c8c6169f155e96e3945d9d1c766e7762e1bc9f
Geni-Wazir <crazycoolbrother@gmail.com>
2023-02-27 22:02:11 +0530

    fixed base issue

--- logarithm.py
+++ logarithm.py
@@ -1,7 +1,8 @@
 import math
 
 def logarithm(number, base):
-    if(number > 0 and base > 0) :
+    if(number > 0 and base > 1) :
         return math.log(number, base)
+    return "The input number shall be greater than zero and base shall be greater than 1"
 
 
